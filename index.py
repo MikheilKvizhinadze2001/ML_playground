334efbfafa30d724ec83e9d22a70e3bfdbe4a474
Mikheil Kvizhinadze <85734592+MikheilKvizhinadze2001@users.noreply.github.com>
2024-05-17 11:50:11 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
 
-st.title('Hello World')
+st.title('Welcome to the ML app')
+st.write('This app is a demo of the machine learning, computer vision and deep learning capabilities using Streamlit library.')
+st.write('The app is divided into three sections: Object Detection, Playground and fraud detection case study. Without further ado, check you all the sections and see the magic of machine learning in action.😊 ')
 
